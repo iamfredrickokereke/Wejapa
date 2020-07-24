194c2761eaaf422e94c188995a40dbf73bf0e979
Kelly online <kellyonline2016@gmail.com>
2020-07-24 23:46:03 +0100

    server files updated

---
+++ Task-1/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const port = process.env.PORT || 3003;
+
+
+http
+    .createServer( (req, res) => {
+        if (req.method === 'GET' && req.url === '/') {
+            res.writeHead(200, { 'Content-Type' : 'text/html'})              
+            res.write('it works') 
+            res.end()                      
+            } 
+
+            res.end('The page route doesn\'t exist') 
+        })
+             
+
+    .listen(port, (error) => {
+                    if (error) {
+                        console.log('Something don shele :' + error)                        
+                    } else {
+                        console.log(`server is listening on http://localhost:${port}`)                      
+                    }
+                })
