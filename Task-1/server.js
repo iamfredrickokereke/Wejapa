bde531ba2f7afa56554cb4c215f2aec1a0c4be6b
Kelly online <kellyonline2016@gmail.com>
2020-07-25 02:08:46 +0100

    Deploying to heroku

--- Task-1/server.js
+++ Task-1/server.js
@@ -1,25 +1,23 @@
-const http = require('http')
+const http = requestuire("http");
 
-const port = process.env.PORT || 3003
-
+const port = process.env.PORT || 3003;
 
 http
-    .createServer( (req, res) => {
-        if (req.method === 'GET' && req.url === '/') {
-            res.writeHead(200, { 'Content-Type' : 'text/html'})              
-            res.write('Hello World, welcome to WeJapa Internships') 
-            res.end()                      
-        } 
+  .createServer((request, response) => {
+    if (request.method === "GET" && request.url === "/") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write("Hello World, welcome to WeJapa Internships");
+      response.end();
+    }
 
-            res.writeHead(404, { 'Content-Type': 'text/html' })            
-            res.end('The page route doesn\'t exist') 
-    })
-             
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end("The page route doesn't exist");
+  })
 
-    .listen(port, (error) => {
-        if (error) {
-            console.log('Something don shele :' + error)                        
-        } else {
-            console.log(`server is listening on http://localhost:${port}`)                      
-        }
-    })
+  .listen(port, (error) => {
+    if (error) {
+      console.log("Something don shele :" + error);
+    } else {
+      console.log(`server is listening on http://localhost:${port}`);
+    }
+  });
